9c8a3e59dce455da0bd237598e7dbea21da7605d
助 <piece.time.1980@gmail.com>
2026-06-10 16:06:15 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_qhU_evnhV5JMWQEe-eeNtYW_fmK1ad0",
+    authDomain: "inspections-6c279.firebaseapp.com",
+    projectId: "inspections-6c279",
+    storageBucket: "inspections-6c279.firebasestorage.app",
+    messagingSenderId: "855218338095",
+    appId: "1:855218338095:web:1e9cfad822255e0fef09da"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {
+    db,
+    collection,
+    addDoc,
+    getDocs,
+    serverTimestamp
+};
