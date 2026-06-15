2ff3081f0f50a5bfaa1f554242ea780a3941d018
助 <piece.time.1980@gmail.com>
2026-06-16 08:50:58 +0900

    Update logs.js

--- logs.js
+++ logs.js
@@ -366,9 +366,12 @@ async function openExcelExportDialog() {
         const arrayBuffer = await response.arrayBuffer();
 
         await workbook.xlsx.load(arrayBuffer);
-
+　　　　workbook.eachSheet((sheet) => {
+    console.log("シート名:", sheet.name);
+});
+        console.log("sheet取得前");
         const sheet = workbook.getWorksheet("月1～年1");
-
+　　　　console.log("sheet=", sheet);
         if (!sheet) {
             alert("シートが見つかりません");
             return;
