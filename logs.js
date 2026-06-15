3465552c0b43ffea7ffc1aa8c90cb46543f3efff
助 <piece.time.1980@gmail.com>
2026-06-15 09:58:10 +0900

    Update logs.js

--- logs.js
+++ logs.js
@@ -11,7 +11,7 @@ document.addEventListener(
     async () => {
 
         await loadLogs();
-
+initializeExportControls();
         document
             .getElementById("searchBtn")
             ?.addEventListener(
