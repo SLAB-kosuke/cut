438a368f8a60be33f31538c7481c0b637e104934
助 <piece.time.1980@gmail.com>
2026-06-16 14:22:10 +0900

    Update logs.js

--- logs.js
+++ logs.js
@@ -375,11 +375,9 @@ async function openExcelExportDialog() {
         const dailySheet =
     workbook.getWorksheet("日常～週");
         
-sheet.getCell("W5").value =
-    Number(month);
-
-dailySheet.getCell("W5").value =
-    Number(month);
+sheet.getCell("W5").value = `${month}月`;
+dailySheet.getCell("W5").value = `${month}月`;
+        
 console.log(
     "W5 value",
     sheet.getCell("W5").value
